e404a48a2afebfb5cc0a308c5dc36c514fe587b4
Chris Dickinson <chris.dickinson@autotrader.co.uk>
2026-03-02 11:16:15 +0000

    add app

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <a href="mail-to:">info@moretype.co.uk</a>
+        </div>
+    )
+}
