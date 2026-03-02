54f80fbdbc3d494c0093189b8e57cb56e4938d40
Chris Dickinson <chris.dickinson@autotrader.co.uk>
2026-03-02 11:20:40 +0000

    updates

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -4,6 +4,9 @@ export const Header = () => {
     return (
         <div className="header">
             <a href="mail-to:">info@moretype.co.uk</a>
+            <div>
+                <h2>New Website, New Fonts coming soon</h2>
+            </div>
         </div>
     )
 }
