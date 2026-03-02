f09b7bf421e6c9c5043a591364cac56fc7166a2d
Chris Dickinson <chris.dickinson@autotrader.co.uk>
2026-03-02 12:18:50 +0000

    updates

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
+import "./header.styles.css"
 
 export const Header = () => {
     return (
         <div className="header">
-            <a href="mail-to:">info@moretype.co.uk</a>
+            <div className="header-content">
+                <Image src="/logo.png" alt="MoreType logo" width={70} height={40} priority />
+                <a href="mailto:info@moretype.co.uk">info@moretype.co.uk</a>
+            </div>
             <div>
-                <h2>New Website, New Fonts coming soon</h2>
+                <h2 className="header-text">New Website, New Fonts coming soon</h2>
             </div>
         </div>
     )
