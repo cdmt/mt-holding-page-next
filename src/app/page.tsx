7494dbbcdc73e31514fbeb6dc81db3eeea0d8578
chris dickinson <chris@moretype.co.uk>
2026-03-02 17:10:29 +0000

    update images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,74 @@
-import styles from "./page.module.css";
+import "./page.css"
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-
-    </div>
+    <section className="fonts">
+      <ul className="fontList">
+          <li>
+              <img src="/fonts/alber.png" alt="" />
+              <a href="https://www.myfonts.com/collections/alber-new-font-moretype?queryId=92327210d668654bc4cf04c4aec037c9&amp;index=universal_search_data&amp;objectIDs=5424458002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/alwyn.png" alt="" />
+              <a href="https://www.myfonts.com/collections/alwyn-new-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5424503002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/alwyn-rd.png" alt="" />
+              <a href="https://www.myfonts.com/collections/alwyn-new-rounded-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5425628002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/bale.png" alt="" />
+              <a href="https://www.myfonts.com/collections/bale-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5425417002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/bale-mono.png" alt="" />
+              <a href="https://www.myfonts.com/collections/bale-mono-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5425443002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/depot.png" alt="" />
+              <a href="https://www.myfonts.com/collections/depot-new-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5424560002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/depot-con.png" alt="" />
+              <a href="https://www.myfonts.com/collections/depot-new-condensed-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5424530002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/faricy.png" alt="" />
+              <a href="https://www.myfonts.com/collections/faricy-new-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5424588002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/hedley.png" alt="" />
+              <a href="https://www.myfonts.com/collections/hedley-new-font-moretype?queryId=a09bed8d66eb09027a8e1fb286c68d7e&amp;eventName=Product%20Clicked&amp;index=universal_search_data&amp;objectIDs=5424674002&amp;positions=1&amp;inline=true&amp;userquery=Hedley" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/hyla.png" alt="" />
+          </li>
+          <li>
+              <img src="/fonts/mic32.png" alt="" />
+              <a href="https://www.myfonts.com/collections/mic-32-new-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5424716002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/mic32-rd.png" alt="" />
+              <a href="https://www.myfonts.com/collections/mic-32-new-rounded-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5425653002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/mic32-st.png" alt="" />
+              <a href="https://www.myfonts.com/collections/mic-32-new-stencil-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5427697002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/rehn.png" alt="" />
+              <a href="https://www.myfonts.com/collections/rehn-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5485004002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/rehn-con.png" alt="" />
+              <a href="https://www.myfonts.com/collections/rehn-condensed-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5485016002" target="_blank">Buy</a>
+          </li>
+          <li>
+              <img src="/fonts/varly.png" alt="" />
+              <a href="https://www.myfonts.com/collections/varly-font-moretype?queryId=0f2875333ab2378df9b1a342201b664d&amp;index=universal_search_data&amp;objectIDs=5427316002" target="_blank">Buy</a>
+          </li>
+      </ul>
+  
+  </section>
   );
 }
